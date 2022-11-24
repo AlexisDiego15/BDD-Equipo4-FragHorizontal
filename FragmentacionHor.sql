0f8f7fb8c13e67c957f9b2bf5db5b664f927db1a
AlexisDiego15 <36042790+AlexisDiego15@users.noreply.github.com>
2022-11-23 23:22:19 -0600

    Add files via upload

--- FragmentacionHor.sql
+++ FragmentacionHor.sql
@@ -7,25 +7,25 @@
 		BEGIN
 		SET NOCOUNT ON;
 
-		DECLARE @strSql1 nvarchar(1000)
-			set @strSql1 = 'select soh.TerritoryID, sum(t.LineTotal) as Total_Venta
-			from AdventureWorks2019.Sales.SalesOrderHeader soh
+		DECLARE @strSql1 nvarchar(1500)
+			set @strSql1 = 'select soh.TerritoryID as Territorio, sum(t.LineTotal) as Total_Venta
+			from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader soh
 			inner join
 			(
 				select salesorderid, productid, orderqty, linetotal
-				from AdventureWorks2019.Sales.SalesOrderDetail sod
+				from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail sod
 				where ProductID in 
 				(
 					select productid
-					from AdventureWorks2019.Production.Product
+					from [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product
 					where ProductSubcategoryID in
 					(
 					select ProductSubcategoryID
-					from AdventureWorks2019.Production.ProductSubcategory
+					from [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductSubcategory
 					where ProductCategoryID in 
 						(
 						select ProductCategoryID
-						from AdventureWorks2019.Production.ProductCategory
+						from [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductCategory
 						where ProductCategoryID = @cat 
 						) 
 					)
@@ -39,14 +39,14 @@
 	  end
 	  go
 
-	  exec sp_uno 4
+	  exec sp_uno 3
 	  go
 	 
 
 	 -------------------------------------------------------------------
 
-
-	 	drop procedure if exists sp_dos
+	 
+	drop procedure if exists sp_dos
 		go
 
 		create procedure sp_dos
@@ -55,16 +55,16 @@
 		SET NOCOUNT ON;
 
 		DECLARE @strSql2 nvarchar(1500)
-			set @strSql2 = 'select top (1) Q.ProductID, Q.Name, Q.cantidad as CantidadTotal, 
-					W.TerritoryID, W.NombreTerritorio, W.cantidadpt as CantidadTerritorio
+			set @strSql2 = 'select top (1) Q.ProductID as ID, Q.Name as Nombre, Q.cantidad as CantidadTotal, 
+					W.TerritoryID as IDTerritorio, W.NombreTerritorio as NTerritotio, W.cantidadpt as CantidadTerritorio
 					 from (
 						 select a.ProductID, b.Name, c.TerritoryID, d.Name as NombreTerritorio, count(*) cantidadpt
-						 from AdventureWorks2019.Sales.SalesOrderDetail a
-						 inner join AdventureWorks2019.Sales.SalesOrderHeader c
+						 from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail a
+						 inner join [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader c
 						 on a.SalesOrderID = c.SalesOrderID
-						 inner join AdventureWorks2019.Production.Product b 
+						 inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product b 
 						 on a.ProductID = b.ProductID
-						 inner join AdventureWorks2019.Sales.SalesTerritory d 
+						 inner join [DESKTOP-A5SBAIV].salesAW.Sales.SalesTerritory d 
 						 on c.TerritoryID = d.TerritoryID
 						 where c.TerritoryID between 1 and 6
 						 group by a.ProductID, c.TerritoryID, b.Name, d.Name
@@ -72,10 +72,10 @@
 					 inner join (
 						 select T.ProductID, T.Name, count(*) cantidad
 						 from (select a.ProductID, b.Name, c.TerritoryID
-							from AdventureWorks2019.Sales.SalesOrderDetail a
-						 inner join AdventureWorks2019.Sales.SalesOrderHeader c
+							from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail a
+						 inner join [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader c
 						 on a.SalesOrderID = c.SalesOrderID
-						 inner join AdventureWorks2019.Production.Product b 
+						 inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product b 
 						 on a.ProductID = b.ProductID
 						 where c.TerritoryID between 1 and 6 ) as T
 						 group by ProductID, Name
@@ -89,22 +89,27 @@
 
 	  exec sp_dos
 	
+	 	
 	 
 	 -----------------------------------------------------------
-	 /*create view tres as*/ 
+	 drop view if exists vw_tres
+	  go
+	 create view vw_tres as 
 			select a.ProductID, a.Name, b.ProductCategoryID, c.Name as Categoria,
 				floor(d.Quantity) as Cantidad, d.LocationID, e.Name as Locacion
 			from
-			AdventureWorks2019.Production.Product a
-			inner join AdventureWorks2019.Production.ProductSubcategory b
+			[DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product a
+			inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductSubcategory b
 			on a.ProductSubcategoryID = b.ProductSubcategoryID
-			inner join AdventureWorks2019.Production.ProductCategory c
+			inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductCategory c
 			on c.ProductCategoryID = b.ProductCategoryID
-			inner join AdventureWorks2019.Production.ProductInventory d
+			inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductInventory d
 			on a.ProductID = d.ProductID
-			inner join AdventureWorks2019.Production.Location e
+			inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Location e
 			on e.LocationID = d.LocationID
+	go
 
+			
 
 drop procedure if exists sp_tres
 		go
@@ -121,14 +126,14 @@ drop procedure if exists sp_tres
 			set @strSql1 = 'update d
 				set d.Quantity = (d.Quantity*1.05)
 				from
-				AdventureWorks2019.Production.ProductInventory d
-				inner join AdventureWorks2019.Production.Product a
+				[DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductInventory d
+				inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product a
 				on a.ProductID = d.ProductID
-				inner join AdventureWorks2019.Production.ProductSubcategory b
+				inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductSubcategory b
 				on a.ProductSubcategoryID = b.ProductSubcategoryID
-				inner join AdventureWorks2019.Production.ProductCategory c
+				inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.ProductCategory c
 				on c.ProductCategoryID = b.ProductCategoryID
-				inner join AdventureWorks2019.Production.Location e
+				inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Location e
 				on e.LocationID = d.LocationID
 				where c.ProductCategoryID=@cat and d.LocationID=@loc
 				'
@@ -140,10 +145,12 @@ drop procedure if exists sp_tres
 	  end
 	  go
 
-	  exec sp_tres 4, 4
+	  exec sp_tres 4, 6
 	  go
+
 	  --------------------------------------------------------------------------
-	  drop procedure if exists sp_cuatro
+	  
+	 drop procedure if exists sp_cuatro
 		go
 
 		create procedure sp_cuatro
@@ -154,8 +161,8 @@ drop procedure if exists sp_tres
 
 		DECLARE @strSql4 nvarchar(500)
 			set @strSql4 = 'select a.SalesOrderID, a.TerritoryID, a.SalesPersonID, b.TerritoryID
-					from AdventureWorks2019.Sales.SalesOrderHeader a
-					inner join AdventureWorks2019.Sales.Customer b
+					from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader a
+					inner join [DESKTOP-A5SBAIV].salesAW.Sales.Customer b
 					on a.CustomerID = b.CustomerID
 					where a.TerritoryID != b.TerritoryID'
 			
@@ -171,7 +178,7 @@ drop procedure if exists sp_tres
 
 
 	  -----------------------------------------------------------------------------
-	  		drop procedure if exists sp_cinco
+	  	drop procedure if exists sp_cinco
 		go
 
 		create procedure sp_cinco
@@ -184,10 +191,10 @@ drop procedure if exists sp_tres
 		DECLARE @RegistrosAfectados INTEGER
 
 		DECLARE @strSql5 nvarchar(500)
-			set @strSql5 = 'update AdventureWorks2019.Sales.SalesOrderDetail 
+			set @strSql5 = 'update [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail 
 				set OrderQty = @cantidad
 				from
-				AdventureWorks2019.Sales.SalesOrderDetail 
+				[DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail 
 				where SalesOrderID = @orden and ProductID = @producto'
 			
 				execute sp_executesql @strSql5, N'@cantidad int, @producto int, @orden int', 
@@ -200,11 +207,17 @@ drop procedure if exists sp_tres
 	  exec sp_cinco 5, 776, 43659
 	  go
 
-	  select SalesOrderID, ProductID, OrderQty 
-	  from AdventureWorks2019.Sales.SalesOrderDetail 
+	  drop view if exists vw_cinco
+	  go
+	  create view vw_cinco as
+	  select s.SalesOrderID, s.ProductID, s.OrderQty, p.Name
+	  from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderDetail s
+	  inner join [DESKTOP-A5SBAIV\SQLSERVERDOS].productionAW.Production.Product p
+	  on p.ProductID = s.ProductID
+	  go
 
 	  -----------------------------------------------------------------------------------------------------
-	  	drop procedure if exists sp_seis
+	  		 drop procedure if exists sp_seis
 		go
 		create procedure sp_seis
 			@metodo int,
@@ -215,10 +228,10 @@ drop procedure if exists sp_tres
 		DECLARE @RegistrosAfectados INTEGER
 
 		DECLARE @strSql6 nvarchar(500)
-			set @strSql6 = 'update AdventureWorks2019.Sales.SalesOrderHeader
+			set @strSql6 = 'update [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader
 				set	ShipMethodID = @metodo
 				from
-				AdventureWorks2019.Sales.SalesOrderHeader 
+				[DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader 
 				where SalesOrderID = @orden'
 			
 				execute sp_executesql @strSql6, N'@metodo int, @orden int', @metodo, @orden
@@ -230,23 +243,31 @@ drop procedure if exists sp_tres
 	  exec sp_seis 3, 43659
 	  go
 
+	  drop view if exists vw_seis
+	  go
+	  create view vw_seis as
 	  select s.SalesOrderID, s.ShipMethodID, m.Name
-	  from AdventureWorks2019.Sales.SalesOrderHeader s
-	  inner join AdventureWorks2019.Purchasing.ShipMethod m
+	  from [DESKTOP-A5SBAIV].salesAW.Sales.SalesOrderHeader s
+	  inner join [DESKTOP-A5SBAIV\SQLSERVERTRES].otrosAW.Purchasing.ShipMethod m
 	  on s.ShipMethodID=m.ShipMethodID
+	  go
 
 	  ------------------------------------------------------------------------------------------------
-	  	
-		
-			 	
-		select e.EmailAddress, p.FirstName, p.LastName, p.PersonType
-		from AdventureWorks2019.Person.EmailAddress e
-		inner join AdventureWorks2019.Person.Person p
+	 			 	
+	drop view if exists vw_siete 
+	go
+	create view vw_siete as
+	  select Correo, Nombre, Apellido from openquery ([DESKTOP-A5SBAIV\SQLSERVERTRES],
+		'select e.EmailAddress as Correo, p.FirstName as Nombre, p.LastName as Apellido, p.PersonType
+		from [DESKTOP-A5SBAIV\SQLSERVERTRES].otrosAW.Person.EmailAddress e
+		inner join [DESKTOP-A5SBAIV\SQLSERVERTRES].otrosAW.Person.Person p
 		on e.BusinessEntityID = p.BusinessEntityID
-		where p.PersonType = 'IN'
+		where p.PersonType = ''IN''')
+	go
 
 
-		drop procedure if exists sp_siete
+
+				drop procedure if exists sp_siete
 		go
 		create procedure sp_siete
 			@emailnuevo nvarchar(50),
@@ -257,12 +278,17 @@ drop procedure if exists sp_tres
 		DECLARE @RegistrosAfectados INTEGER
 
 		DECLARE @strSql7 nvarchar(900)
-			set @strSql7 = 'update e
+			set @strSql7 ='
+				update e
 				set e.EmailAddress = @emailnuevo
-				from AdventureWorks2019.Person.EmailAddress e
-				inner join AdventureWorks2019.Person.Person p
-				on e.BusinessEntityID = p.BusinessEntityID
-				where p.PersonType = '+'''IN'''+' and e.EmailAddress = @emailanterior'
+				from openquery ([DESKTOP-A5SBAIV\SQLSERVERTRES],
+					''select BusinessEntityID, EmailAddress
+					from [DESKTOP-A5SBAIV\SQLSERVERTRES].otrosAW.Person.EmailAddress'') e
+				join openquery ([DESKTOP-A5SBAIV\SQLSERVERTRES],
+					''select FirstName, LastName, PersonType, BusinessEntityID
+					from [DESKTOP-A5SBAIV\SQLSERVERTRES].otrosAW.Person.Person'') p
+				on e.BusinessEntityID = p.BusinessEntityID	
+				where p.PersonType = ''IN'' and e.EmailAddress = @emailanterior' 
 			
 				execute sp_executesql @strSql7, N'@emailnuevo nvarchar(50), @emailanterior nvarchar(50)', 
 													@emailnuevo, @emailanterior
@@ -273,6 +299,3 @@ drop procedure if exists sp_tres
   
 	  exec sp_siete 'redfoodiego@gmail.com', 'rebecca3@adventure-works.com'
 	  go
-
-
-
